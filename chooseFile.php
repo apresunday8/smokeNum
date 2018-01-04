60085180187756084469b61614e3ac53ea0c8113
wwd <xiangqiba@icloud.com>
2018-01-04 14:38:24 +0800

    第二次大的修改

--- chooseFile.php
+++ chooseFile.php
@@ -9,12 +9,12 @@ $brr = (int)mysql_fetch_assoc($result);
 	function qwe(){
 	var asd = <?php echo $brr ?>;
 	if(asd==0){
-		alert("请先上传数据后再操作");
+		alert("亲，你还没上传文件的呦~");
 	}else{
 	var myselect = document.getElementById("files");       //获取select对象
 	var index = myselect.selectedIndex;					   //获取选中项的索引
 	if(myselect.options[index].value == 0){                //获取选中项的值
-		alert("请选择文件后再进行操作");
+		alert("您现在需要选择文件才能开始喔~");
 		exit();
 	}else{
 		var tableSuffix = myselect.options[index].value;
--- test.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$val = "student";
-$val2 = 1;
-$val3 = "student".$val2;
-echo $val3;
-?>
